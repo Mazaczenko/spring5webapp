6c3db9f7e46bd6f95a3ce357e560f617babe5c72
Wojciech Mazur <wojtekemmm@gmail.com>
2020-05-20 00:41:37 +0200

    s2_ex1

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -5,15 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Created by jt on 12/24/19.
- */
 @Controller
 public class BookController {
 
     private final BookRepository bookRepository;
 
     public BookController(BookRepository bookRepository) {
+
         this.bookRepository = bookRepository;
     }
 
